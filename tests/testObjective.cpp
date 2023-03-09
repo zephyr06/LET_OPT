019a69f7a65e0b93a64ef11fd4f61a6d0efe128d
zephyr06 <sen.zephyr.wang@gmail.com>
2023-03-09 18:15:06 -0500

    add GetFirstReactJob and ObXXX

--- tests/testObjective.cpp
+++ tests/testObjective.cpp
@@ -57,14 +57,76 @@ class ObjReactionTime : public ObjectiveFunctionBase {
                       const ChainPermutation &chain_perm);
 };
 
+// task_index_in_chain: the index of a task in a cause-effect chain
+// for example: consider a chain 0->1->2, task0's index is 0, task1's index is
+// 1, task2's index is 2;
+JobCEC GetFirstReactJobWithSuperPeriod(const JobCEC &job_curr,
+                                       const ChainPermutation &chain_perm,
+                                       uint task_index_in_chain) {
+    if (task_index_in_chain < chain_perm.size()) {
+        const SinglePairPermutation &pair_perm_curr =
+            chain_perm[task_index_in_chain];
+        // pair_perm_curr.print();
+        auto itr = pair_perm_curr.job_matches_.find(job_curr);
+        if (itr == pair_perm_curr.job_matches_.end())
+            CoutError(
+                "Didn't find job_curr records in "
+                "GetFirstReactJobWithSuperPeriod!");
+        else
+            return itr->second
+                .front();  // assume it is sorted, TODO: guarantee it
+    } else {
+        CoutError("No match because of an out-of-range error!");
+    }
+
+    return JobCEC(-1, -1);
+}
+
+JobCEC GetFirstReactJob(const JobCEC &job_curr,
+                        const ChainPermutation &chain_perm,
+                        uint task_index_in_chain,
+                        const TaskSetInfoDerived &tasks_info) {
+    const SinglePairPermutation &pair_perm_curr =
+        chain_perm[task_index_in_chain];
+    int task_id_prev = job_curr.taskId;
+    if (task_id_prev != pair_perm_curr.inequality_.task_prev_id_)
+        CoutError("Wrong task_index_in_chain index in GetFirstReactJob!");
+    int task_id_next = pair_perm_curr.inequality_.task_next_id_;
+
+    int super_period = GetSuperPeriod(tasks_info.tasks[task_id_prev],
+                                      tasks_info.tasks[task_id_next]);
+    int prev_jobs_in_sp = super_period / tasks_info.tasks[task_id_prev].period;
+    int next_jobs_in_sp = super_period / tasks_info.tasks[task_id_next].period;
+
+    int sp_index = job_curr.jobId / prev_jobs_in_sp;
+    JobCEC react_job = GetFirstReactJobWithSuperPeriod(
+        JobCEC(job_curr.taskId, job_curr.jobId % prev_jobs_in_sp), chain_perm,
+        task_index_in_chain);
+    react_job.jobId += sp_index * next_jobs_in_sp;
+    return react_job;
+}
+
 const std::string ObjReactionTime::type_trait("ReactionTime");
 double ObjReactionTime::Obj(const DAG_Model &dag_tasks,
                             const TaskSetInfoDerived &tasks_info,
                             const ChainPermutation &chain_perm) {
-    for (uint i = 0; i < tasks_info.sizeOfVariables[chain_perm.GetTaskId(0)];
-         i++)
-        ;
-    return 0;
+    int max_reaction_time = -1;
+    for (uint j = 0; j < tasks_info.sizeOfVariables[chain_perm.GetTaskId(0)];
+         j++)  // iterate each source job within a hyper-period
+    {
+        JobCEC job_source(chain_perm.GetTaskId(0),
+                          j);  // TODO: fix the hyper-period mapping issue
+        JobCEC job_curr = job_source;
+        for (uint i = 0; i < chain_perm.size();
+             i++)  // iterate through task-level cause-effect chain
+        {
+            job_curr = GetFirstReactJob(job_curr, chain_perm, i, tasks_info);
+        }
+        max_reaction_time = std::max(
+            max_reaction_time, int(GetDeadline(job_curr, tasks_info) -
+                                   GetActivationTime(job_source, tasks_info)));
+    }
+    return max_reaction_time;
 }
 
 class PermutationTest1 : public ::testing::Test {
@@ -85,6 +147,12 @@ class PermutationTest1 : public ::testing::Test {
 
         perm01 = TwoTaskPermutations(0, 1, tasks_info);
         perm12 = TwoTaskPermutations(1, 2, tasks_info);
+
+        perm01[0].print();
+        perm01[1].print();
+        perm01[2].print();
+        perm12[0].print();
+        perm12[1].print();
     };
 
     DAG_Model dag_tasks;
@@ -111,31 +179,52 @@ TEST_F(PermutationTest1, GetTaskId) {
     EXPECT_EQ(2, chain_perm.GetTaskId(2));
 }
 
-// TEST_F(PermutationTest1, ChainPermutation_v1) {
-//     // chain is 0 -> 1 -> 2
-//     TwoTaskPermutations perm01(0, 1, tasks_info);
-//     TwoTaskPermutations perm12(1, 2, tasks_info);
-//     perm01[0].print();
-//     perm01[1].print();
-//     perm01[2].print();
-//     perm12[0].print();
-//     perm12[1].print();
+TEST_F(PermutationTest1, GetFirstReactJob) {
+    // chain is 0 -> 1 -> 2
+    TwoTaskPermutations perm01(0, 1, tasks_info);
+    TwoTaskPermutations perm12(1, 2, tasks_info);
 
-//     ChainPermutation chain_perm;
-//     chain_perm.push_back(perm01[0]);
-//     chain_perm.push_back(perm12[0]);
-//     EXPECT_EQ(20, ObjReactionTime::Obj(dag_tasks, tasks_info, chain_perm));
+    ChainPermutation chain_perm;
+    chain_perm.push_back(perm01[0]);
+    chain_perm.push_back(perm12[0]);
 
-//     chain_perm.permutation_chain_.clear();
-//     chain_perm.push_back(perm01[0]);
-//     chain_perm.push_back(perm12[1]);
-//     EXPECT_EQ(40, ObjReactionTime::Obj(dag_tasks, tasks_info, chain_perm));
+    // with the same super-period
+    EXPECT_EQ(JobCEC(1, 0),
+              GetFirstReactJobWithSuperPeriod(JobCEC(0, 0), chain_perm, 0));
+    EXPECT_EQ(JobCEC(1, 0),
+              GetFirstReactJobWithSuperPeriod(JobCEC(0, 1), chain_perm, 0));
+    EXPECT_EQ(JobCEC(2, 0),
+              GetFirstReactJobWithSuperPeriod(JobCEC(1, 0), chain_perm, 1));
 
-//     chain_perm.permutation_chain_.clear();
-//     chain_perm.push_back(perm01[1]);
-//     chain_perm.push_back(perm12[1]);
-//     EXPECT_EQ(50, ObjReactionTime::Obj(dag_tasks, tasks_info, chain_perm));
-// }
+    // span super-periods
+    EXPECT_EQ(JobCEC(1, 1),
+              GetFirstReactJob(JobCEC(0, 2), chain_perm, 0, tasks_info));
+    EXPECT_EQ(JobCEC(1, 1),
+              GetFirstReactJob(JobCEC(0, 3), chain_perm, 0, tasks_info));
+    EXPECT_EQ(JobCEC(2, 1),
+              GetFirstReactJob(JobCEC(1, 1), chain_perm, 1, tasks_info));
+}
+
+TEST_F(PermutationTest1, ChainPermutation_v1) {
+    // chain is 0 -> 1 -> 2
+    TwoTaskPermutations perm01(0, 1, tasks_info);
+    TwoTaskPermutations perm12(1, 2, tasks_info);
+
+    ChainPermutation chain_perm;
+    chain_perm.push_back(perm01[0]);
+    chain_perm.push_back(perm12[0]);
+    EXPECT_EQ(20, ObjReactionTime::Obj(dag_tasks, tasks_info, chain_perm));
+
+    chain_perm.permutation_chain_.clear();
+    chain_perm.push_back(perm01[0]);
+    chain_perm.push_back(perm12[1]);
+    EXPECT_EQ(40, ObjReactionTime::Obj(dag_tasks, tasks_info, chain_perm));
+
+    chain_perm.permutation_chain_.clear();
+    chain_perm.push_back(perm01[1]);
+    chain_perm.push_back(perm12[1]);
+    EXPECT_EQ(50, ObjReactionTime::Obj(dag_tasks, tasks_info, chain_perm));
+}
 
 int main(int argc, char **argv) {
     // ::testing::InitGoogleTest(&argc, argv);
