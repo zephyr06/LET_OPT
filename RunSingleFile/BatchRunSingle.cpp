b09d9c6debde345872c3455130aceffc7eda4aa9
zephyr06 <sen.zephyr.wang@gmail.com>
2023-03-12 21:19:31 -0400

    batchOptimize.h selects folders to optimize

--- RunSingleFile/BatchRunSingle.cpp
+++ RunSingleFile/BatchRunSingle.cpp
@@ -10,10 +10,18 @@ int main(int argc, char *argv[]) {
         .help("the folder of task sets to run experiments")
         .scan<'i', int>();
 
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::runtime_error &err) {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
     int N = program.get<int>("--N");
 
     std::vector<DAG_SPACE::BaselineMethods> baselineMethods = {
         DAG_SPACE::InitialMethod, DAG_SPACE::TOM};  // , DAG_SPACE::TOM_FAST
     DAG_SPACE::BatchOptimizeOrder<DAG_SPACE::ObjReactionTime>(baselineMethods,
                                                               N);
+    std::cout << "N: " << N << "\n";
 }
