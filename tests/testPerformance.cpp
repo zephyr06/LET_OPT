e0ab14f7c5e0f01400cb788decb70b2d6343117d
zephyr06 <sen.zephyr.wang@gmail.com>
2023-03-17 16:24:51 -0400

    add testPerformance

--- tests/testPerformance.cpp
+++ tests/testPerformance.cpp
@@ -5,14 +5,11 @@ using namespace DAG_SPACE;
 TEST(average_performance, v1) {
     BeginTimer("main");
 
-    std::string dataSetFolder =
-        GlobalVariablesDAGOpt::PROJECT_PATH + "TaskData/dagTasksPerfTest/";
-    ClearResultFiles(dataSetFolder);
     std::vector<DAG_SPACE::BaselineMethods> baselineMethods = {
         DAG_SPACE::InitialMethod, DAG_SPACE::TOM};
     auto res = DAG_SPACE::BatchOptimizeOrder<DAG_SPACE::ObjReactionTime>(
         baselineMethods, 0, 2);
-    EXPECT_THAT(res[BaselineMethods::TOM].performance, testing::Le(0.53));
+    EXPECT_THAT(res[BaselineMethods::TOM].performance, testing::Le(0.385));
 
     EXPECT_DOUBLE_EQ(1.0, res[BaselineMethods::TOM].schedulableRatio);
     EndTimer("main");
