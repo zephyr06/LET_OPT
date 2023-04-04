4976a19e3e440372e0c7876ac1fe42ba5161715e
zephyr06 <sen.zephyr.wang@gmail.com>
2023-04-04 19:48:00 -0400

    submit multiple tasks in arc

---
+++ Experiments/BatchRT_Approx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -J hello-world
+#SBATCH --account=ev_charging
+#SBATCH --partition=normal_q
+#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=1  
+#SBATCH --time=0-00:20:00 # 20 minutes
+
+#SBATCH --mail-user=swang666@vt.edu
+#SBATCH --mail-type=BEGIN  # send email when job begins
+#SBATCH --mail-type=END    # send email when job ends
+#SBATCH --mail-type=FAIL   # send email when job aborts
+
+
+module load CMake/3.15.3-GCCcore-8.3.0
+module load boost/1.79 
+module load Eigen/3.3.7
+module load yaml-cpp/0.6.3-GCCcore-8.3.0
+
+cd ~/Programming/LET_OPT/Experiments
+N=$1
+file_index=$2
+end_index=$3
+./RunSingleFile/BatchRunSingle.run --N "$N" --begin "$file_index" --end "$end_index" 
---
+++ Experiments/BatchRT_Arc.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+MinTaskNumber=5
+MaxTaskNumber=5
+
+MinFileIndex=0
+MaxFileIndex=999
+FILES_PER_CPU=200
+
+
+cd ~/Programming/LET_OPT/release
+cmake -DCMAKE_BUILD_TYPE=RELEASE ..
+make BatchRunSingle.run -j8
+
+perform_optimization() {
+	# Optimize energy consumption
+	# for
+	# ./RunSingle/BatchRunSingle.run --N $1
+	# ./tests/BatchControl $1
+	# cd $ROOT_PATH/CompareWithBaseline
+	for file_index in $(seq $MinFileIndex $FILES_PER_CPU $MaxFileIndex); do
+	end_index=$((file_index + FILES_PER_CPU))
+	echo "Processing file_index=$file_index:$1:$end_index"
+	# ./RunSingleFile/BatchRunSingle.run --N "$1" --begin "$file_index" --end "$end_index" 
+	./BatchRT_Approx.sh $1 $file_index $end_index
+	done
+	wait
+}
+
+for (( task_number=$MinTaskNumber; task_number<=$MaxTaskNumber; task_number++ ))
+do
+echo "Processing N=$task_number:"
+	perform_optimization $task_number
+done
+
