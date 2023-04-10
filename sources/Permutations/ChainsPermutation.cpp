8b51ab3fd74f7e133744ed99f8a5ef55e690216a
zephyr06 <sen.zephyr.wang@gmail.com>
2023-04-10 13:01:26 -0400

    add new impl for IsTwoPermConflicted_SerialConnect

--- sources/Permutations/ChainsPermutation.cpp
+++ sources/Permutations/ChainsPermutation.cpp
@@ -136,30 +136,18 @@ bool IsTwoPermConflicted_SameSink(const VariableRange& variable_od_range,
 }
 
 // return true if conflicted, false if safe
+// TODO: pass rta as arguments
 bool IsTwoPermConflicted_SerialConnect(const VariableRange& variable_od_range,
                                        const SinglePairPermutation& perm_prev,
                                        const SinglePairPermutation& perm_curr) {
   if (perm_prev.GetNextTaskId() == perm_curr.GetPrevTaskId()) {
-    int deadline_prev_min =
-        variable_od_range.lower_bound.at(perm_prev.GetPrevTaskId())
-            .deadline;  // this is rta
-    int offset_curr_min =
-        deadline_prev_min - perm_prev.inequality_.upper_bound_;
-
-    int offset_next_max =
-        variable_od_range.upper_bound.at(perm_curr.GetNextTaskId()).offset;
-    int deadline_curr_max_from_ineq =
-        offset_next_max + perm_curr.inequality_.upper_bound_;
-
-    int deadline_curr_max =
-        variable_od_range.upper_bound.at(perm_curr.GetPrevTaskId()).deadline;
-    deadline_curr_max =
-        std::min(deadline_curr_max, deadline_curr_max_from_ineq);
-
+    Interval offset_curr_range =
+        GetOffsetRange_RTPerm(variable_od_range, perm_prev.inequality_);
+    Interval deadline_curr_range =
+        GetDeadlineRange_RTPerm(variable_od_range, perm_curr.inequality_);
     int rta_curr =
         variable_od_range.lower_bound.at(perm_curr.GetPrevTaskId()).deadline;
-    int offset_curr_max = deadline_curr_max - rta_curr;
-    if (offset_curr_max < offset_curr_min) {
+    if (offset_curr_range.start + rta_curr > deadline_curr_range.getFinish()) {
       return true;
     }
   }
