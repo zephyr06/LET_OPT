9e1be60f20fbfa8c959bcf1a5cd8e37058f9ffe8
Dong Li <dongli@vt.edu>
2023-04-16 17:09:02 -0400

    add backup bash to backup all results in /TaskData/N* and all ARC log files

---
+++ Experiments/backups/backupresults.sh
@@ -0,0 +1,17 @@
+ROOT_PATH=/projects/rtss_let/LET_OPT
+ROOT_PATH=/home/dong/workspace/LET_OPT
+
+backup_file_name=ARC_Backup-$(date +"%Y%m%d%H%M%S")
+echo "backup in $ROOT_PATH/Experiments/backups/${backup_file_name}.tar.gz"
+cd $ROOT_PATH/Experiments/backups
+
+mkdir $backup_file_name
+cp -r $ROOT_PATH/TaskData/N* ./$backup_file_name
+rm ./$backup_file_name/*.zip
+
+cp -r $ROOT_PATH/Experiments/dong ./$backup_file_name/slurm_outputs/
+cp $ROOT_PATH/Experiments/*.out ./$backup_file_name/slurm_outputs/
+
+tar -czf $backup_file_name.tar.gz $backup_file_name
+
+rm $backup_file_name -r
