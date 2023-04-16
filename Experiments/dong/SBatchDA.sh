eb74035fd25e82d73ce01eec4004e70b7417fdaa
Dong Li <dongli@vt.edu>
2023-04-15 22:52:34 -0400

    update some bash files for ARC

--- Experiments/dong/SBatchDA.sh
+++ Experiments/dong/SBatchDA.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-#SBATCH -J BatchRunAll $1 $2 $3
+# SBATCH -J BatchRunAll
 #SBATCH --account=ev_charging
 #SBATCH --partition=normal_q
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
 #SBATCH --time=0-00:60:00 # 60 minutes
 
-#SBATCH --mail-user=swang666@vt.edu
+#SBATCH --mail-user=dongli@vt.edu
 #SBATCH --mail-type=BEGIN  # send email when job begins
 #SBATCH --mail-type=END    # send email when job ends
 #SBATCH --mail-type=FAIL   # send email when job aborts
 
 module reset
-module load CMake/3.15.3-GCCcore-8.3.0
-module load boost/1.68.0.a 
-module load Eigen/3.3.7-GCCcore-8.3.0
+# module load CMake/3.15.3-GCCcore-8.3.0
+# module load boost/1.68.0.a 
+# module load Eigen/3.3.7-GCCcore-8.3.0
 module load yaml-cpp/0.6.3-GCCcore-8.3.0
 
 
-ROOT_PATh=/projects/rtss_let/LET_OPT
+ROOT_PATH=/projects/rtss_let/LET_OPT
 
-cd $ROOT_PATh/release
-cmake -DCMAKE_BUILD_TYPE=RELEASE ..
-make BatchRunAll.run 
+cd $ROOT_PATH/release
+# cmake -DCMAKE_BUILD_TYPE=RELEASE ..
+# make BatchRunAll.run 
 N=$1
 file_index=$2
 end_index=$3
-./RunSingleFile/BatchRunAll.run --N "$N" --begin "$file_index" --end "$end_index" 
+./RunSingleFile/BatchDAWithMartRunAll.run --N "$N" --begin "$file_index" --end "$end_index" 
