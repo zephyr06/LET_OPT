a0c1703fca9aec913311f24ff36a07e5703c7573
zephyr06 <sen.zephyr.wang@gmail.com>
2023-04-17 18:49:04 -0400

    add ObjPerChain

--- sources/ObjectiveFunction/ObjectiveFunctionBase.cpp
+++ sources/ObjectiveFunction/ObjectiveFunctionBase.cpp
@@ -5,6 +5,20 @@ namespace DAG_SPACE {
 const std::string ObjectiveFunctionBaseIntermediate::type_trait(
     "ObjectiveFunctionBase");
 
+std::vector<double> ObjectiveFunctionBaseIntermediate::ObjPerChain(
+    const DAG_Model &dag_tasks, const TaskSetInfoDerived &tasks_info,
+    const ChainsPermutation &chains_perm, const VariableOD &variable_od,
+    const std::vector<std::vector<int>> &chains_to_analyze) {
+  std::vector<double> obj_per_chain;
+  obj_per_chain.reserve(chains_to_analyze.size());
+  for (const auto &chain : chains_to_analyze) {
+    if (chain.size() == 0) continue;
+    obj_per_chain.push_back(
+        ObjSingleChain(dag_tasks, tasks_info, chains_perm, chain, variable_od));
+  }
+  return obj_per_chain;
+}
+
 double ObjectiveFunctionBaseIntermediate::Obj(
     const DAG_Model &dag_tasks, const TaskSetInfoDerived &tasks_info,
     const ChainsPermutation &chains_perm, const VariableOD &variable_od,
@@ -13,12 +27,9 @@ double ObjectiveFunctionBaseIntermediate::Obj(
   BeginTimer(__FUNCTION__);
 #endif
 
-  int max_obj = 0;
-  for (const auto &chain : chains_to_analyze) {
-    if (chain.size() == 0) continue;
-    max_obj +=
-        ObjSingleChain(dag_tasks, tasks_info, chains_perm, chain, variable_od);
-  }
+  std::vector<double> obj_vec = ObjPerChain(dag_tasks, tasks_info, chains_perm,
+                                            variable_od, chains_to_analyze);
+  int max_obj = std::accumulate(obj_vec.begin(), obj_vec.end(), 0);
 
 #ifdef PROFILE_CODE
   EndTimer(__FUNCTION__);
--- sources/ObjectiveFunction/ObjectiveFunctionBase.h
+++ sources/ObjectiveFunction/ObjectiveFunctionBase.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "sources/Permutations/ChainsPermutation.h"
 #include "sources/Optimization/Variable.h"
+#include "sources/Permutations/ChainsPermutation.h"
 #include "sources/Utils/profilier.h"
 namespace DAG_SPACE {
 
@@ -16,6 +16,11 @@ class ObjectiveFunctionBaseIntermediate {
     CoutError("Base function should not be called!");
     return 0;
   }
+
+  std::vector<double> ObjPerChain(
+      const DAG_Model &dag_tasks, const TaskSetInfoDerived &tasks_info,
+      const ChainsPermutation &chains_perm, const VariableOD &variable_od,
+      const std::vector<std::vector<int>> &chains_to_analyze);
 
   double Obj(const DAG_Model &dag_tasks, const TaskSetInfoDerived &tasks_info,
              const ChainsPermutation &chains_perm,
