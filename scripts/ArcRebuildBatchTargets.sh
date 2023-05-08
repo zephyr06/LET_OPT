777863ef1afc73f53fe552d670e3bd8c16d202e8
Dong Li <dongli@vt.edu>
2023-05-08 15:33:08 -0400

    rerun results of RTDA on 20220422 task set

---
+++ scripts/ArcRebuildBatchTargets.sh
@@ -0,0 +1,13 @@
+ROOT_PATH=/projects/rtss_let/LET_OPT
+
+cd $ROOT_PATH/release
+module reset
+module load yaml-cpp/0.6.3-GCCcore-8.3.0
+
+module load CMake/3.15.3-GCCcore-8.3.0
+module load boost/1.68.0.a 
+module load Eigen/3.3.7-GCCcore-8.3.0
+cmake -DCMAKE_BUILD_TYPE=RELEASE ..
+make BatchRunAll.run -j16 
+make BatchRunSF.run -j16
+make BatchDAWithMartRunAll.run -j16
