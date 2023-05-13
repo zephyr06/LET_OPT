d491b5f2810294d2d32ed97f516cb3e68022b3c3
Dong Li <dongli@vt.edu>
2023-05-12 21:14:34 -0400

    update all figure legends

--- CompareWithBaseline/GlobalVariables.py
+++ CompareWithBaseline/GlobalVariables.py
@@ -3,18 +3,52 @@ baseline_method_names = ["InitialMethod", "ImplicitCommunication", "TOM_BF", "TO
                          "TOM_Sort", "Martinez18", "TOM_Sort_Offset", "Bardatsch16"]
 baseline_method_labels = {"InitialMethod": "StanLET",
                           "ImplicitCommunication": "Implicit",
-                          "TOM_BF": "eLET_BF",
-                          "TOM_WSkip": "eLET_WSkip",
-                          "TOM_Sort": "eLET_Sort",
+                          "TOM_BF": "iLET_BF",
+                          "TOM_WSkip": "iLET_WSkip",
+                          "TOM_Sort": "iLET_Sort",
                           "Martinez18": "Martinez18",
-                          "TOM_Sort_Offset": "eLET_Sort_Offset",
+                          "TOM_Sort_Offset": "Martinez18_Algo1",
                           "Bardatsch16": "Bradatsch16"}
 
-marker_list = ["o", "*", "D", "^", "s", "D", "P", "X"]
-markersize_list = [8, 12, 10, 12, 8, 8, 8, 4]
-# markersize_list = [6, 10, 6, 8, 6, 6, 6, 3]
-alpha_list = [1, 1, 1, 1, 1, 1, 1, 1]
-color_list = ["#0084DB", "#ffa64d", "r", "y", "limegreen", "purple", "k", "#00FFFF"]
+marker_map = {"InitialMethod": "o",
+              "ImplicitCommunication": "*",
+              "TOM_BF": "D",
+              "TOM_WSkip": "^",
+              "TOM_Sort": "s",
+              "Martinez18": "D",
+              "TOM_Sort_Offset": "P",
+              "Bardatsch16": "X"}
+marker_size_map = {"InitialMethod": 8,
+                   "ImplicitCommunication": 12,
+                   "TOM_BF": 10,
+                   "TOM_WSkip": 12,
+                   "TOM_Sort": 8,
+                   "Martinez18": 8,
+                   "TOM_Sort_Offset": 8,
+                   "Bardatsch16": 4}
+# marker_size_map = {"InitialMethod": 6,
+#                    "ImplicitCommunication": 10,
+#                    "TOM_BF": 6,
+#                    "TOM_WSkip": 8,
+#                    "TOM_Sort": 6,
+#                    "Martinez18": 6,
+#                    "TOM_Sort_Offset": 6,
+#                    "Bardatsch16": 3}
+color_map = {"InitialMethod": "#0084DB",
+             "ImplicitCommunication": "#ffa64d",
+             "TOM_BF": "r",
+             "TOM_WSkip": "y",
+             "TOM_Sort": "limegreen",
+             "Martinez18": "purple",
+             "TOM_Sort_Offset": "k",
+             "Bardatsch16": "#00FFFF"}
+
+# marker_list = ["o", "*", "D", "^", "s", "D", "P", "X"]
+# markersize_list = [8, 12, 10, 12, 8, 8, 8, 4]
+# # markersize_list = [6, 10, 6, 8, 6, 6, 6, 3]
+# alpha_list = [1, 1, 1, 1, 1, 1, 1, 1]
+# color_list = ["#0084DB", "#ffa64d", "r", "y",
+#               "limegreen", "purple", "k", "#00FFFF"]
 
 # ROOT_PATH = "/home/zephyr/Programming/LET_OPT/"
 # ROOT_CompareWithBaseline_PATH = "/home/zephyr/Programming/LET_OPT/CompareWithBaseline/"
--- CompareWithBaseline/PlotResults.py
+++ CompareWithBaseline/PlotResults.py
@@ -3,6 +3,7 @@ from Read_ScheduleRes import *
 import sys
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 sys.path.insert(1, '~/programming/LET_OPT/CompareWithBaseline')
 
@@ -14,9 +15,10 @@ def plot_Obj_results(task_set_number_range, method_names, obj, exclude_time_out=
     plt.figure()
     ax = plt.subplot(111)
     for i in range(len(method_names)):
-        splot = sns.lineplot(data=dataset_pd_obj, x="index", y=method_names[i], marker=marker_list[i],
-                             color=color_list[i], label=baseline_method_labels[method_names[i]],
-                             markersize=markersize_list[i]) #, alpha=alpha_list[i])
+        splot = sns.lineplot(data=dataset_pd_obj, x="index", y=method_names[i], marker=marker_map[method_names[i]],
+                             color=color_map[method_names[i]
+                                             ], label=baseline_method_labels[method_names[i]],
+                             markersize=marker_size_map[method_names[i]])  # , alpha=alpha_list[i])
     font_size = 15
     plt.xlabel("Task Number", fontsize=font_size)
     plt.ylabel("Relative performance gap(%)", fontsize=font_size)
@@ -25,21 +27,23 @@ def plot_Obj_results(task_set_number_range, method_names, obj, exclude_time_out=
     # else:
     #     TODO set a suitable range for sensor fusion
     #     splot.set_ylim([0.15, 2.05])
-    
 
     # Shrink current axis's height by 10% at the botom
     box = ax.get_position()
     ax.set_position([box.x0, box.y0 + box.height * 0.21,
-                    box.width, box.height * 0.9])
+                     box.width, box.height * 0.9])
+    handles, labels = ax.get_legend_handles_labels()
+    handles = np.concatenate((handles[::3],handles[1::3],handles[2::3]),axis=0)
+    labels = np.concatenate((labels[::3],labels[1::3],labels[2::3]),axis=0)
     if obj == "DataAge":
-        plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.39), ncol=3)
+        plt.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, -0.39), ncol=3)
     elif obj == "ReactionTime":
-        plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
+        plt.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
     elif obj == "SensorFusion":
-        plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
+        plt.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
     else:
         plt.legend()
-        
+
     plt.grid(linestyle="--")
     plt.savefig(ROOT_CompareWithBaseline_PATH + obj +
                 "/Compare_Performance_" + obj + ".pdf", format='pdf')
@@ -53,25 +57,29 @@ def plot_Runtime_results(task_set_number_range, method_names, obj, exclude_time_
     plt.figure()
     ax = plt.subplot(111)
     for i in range(len(method_names)):
-        splot = sns.lineplot(data=dataset_pd_runtime, x="index", y=method_names[i], marker=marker_list[i],
-                             color=color_list[i], label=baseline_method_labels[method_names[i]],
-                             markersize=markersize_list[i]) #, alpha=alpha_list[i])
+        splot = sns.lineplot(data=dataset_pd_runtime, x="index", y=method_names[i], marker=marker_map[method_names[i]],
+                             color=color_map[method_names[i]
+                                             ], label=baseline_method_labels[method_names[i]],
+                             markersize=marker_size_map[method_names[i]])  # , alpha=alpha_list[i])
     font_size = 15
     plt.xlabel("Task Number", fontsize=font_size)
     plt.ylabel("Running time (seconds)", fontsize=font_size)
     splot.set_ylim([1e-6, 1e3])
     splot.set(yscale="log")
-    
+
     # Shrink current axis's height by 10% at the botom
     box = ax.get_position()
     ax.set_position([box.x0, box.y0 + box.height * 0.21,
-                    box.width, box.height * 0.9])
+                     box.width, box.height * 0.9])
+    handles, labels = ax.get_legend_handles_labels()
+    handles = np.concatenate((handles[::3],handles[1::3],handles[2::3]),axis=0)
+    labels = np.concatenate((labels[::3],labels[1::3],labels[2::3]),axis=0)
     if obj == "DataAge":
-        plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.39), ncol=3)
+        plt.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, -0.39), ncol=3)
     elif obj == "ReactionTime":
-        plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
+        plt.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
     elif obj == "SensorFusion":
-        plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
+        plt.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, -0.32), ncol=3)
     else:
         plt.legend()
     plt.grid(linestyle="--")
@@ -89,14 +97,16 @@ def draw_RT_results(task_set_number_range):
 
 
 def draw_DA_results(task_set_number_range):
-    method_names = ["InitialMethod", "ImplicitCommunication", "TOM_BF", "TOM_WSkip",
-                    "TOM_Sort", "Martinez18", "TOM_Sort_Offset", "Bardatsch16"]
+    method_names = ["InitialMethod", "ImplicitCommunication", "Martinez18", "TOM_Sort_Offset",
+                    "Bardatsch16", "TOM_BF", "TOM_WSkip", "TOM_Sort"]
     plot_Obj_results(task_set_number_range, method_names, "DataAge")
     plot_Runtime_results(task_set_number_range, method_names, "DataAge")
 
 
-def draw_SF_results(task_set_number_range, exclude_time_out = False):
+def draw_SF_results(task_set_number_range, exclude_time_out=False):
     method_names = ["InitialMethod",
                     "ImplicitCommunication", "TOM_BF", "TOM_WSkip"]
-    plot_Obj_results(task_set_number_range, method_names, "SensorFusion", exclude_time_out)
-    plot_Runtime_results(task_set_number_range, method_names, "SensorFusion", exclude_time_out)
+    plot_Obj_results(task_set_number_range, method_names,
+                     "SensorFusion", exclude_time_out)
+    plot_Runtime_results(task_set_number_range, method_names,
+                         "SensorFusion", exclude_time_out)
