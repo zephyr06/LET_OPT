82b71f305f090880caa9b7fce6f72fcfe082280c
Dong Li <dongli@vt.edu>
2023-10-03 21:42:22 -0400

    backup high utilization tasksets and results (N5 - N17)

--- scripts/countGeneratedTaskSet.sh
+++ scripts/countGeneratedTaskSet.sh
@@ -2,6 +2,7 @@
 COUNT_PATH="/home/dong/workspace/LET_OPT/TaskData"
 COUNT_PATH=/home/zephyr/Programming/LET_OPT/TaskData
 COUNT_PATH="/home/dong/workspace/LET_OPT/release/generatedNewTaskset"
+COUNT_PATH="/projects/rtss_let/LET_OPT/release/generatedNewTaskset"
 
 echo current results count in dir: $COUNT_PATH
 
