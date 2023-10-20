e79f609d424baf52c531ce474c074a2f3b674abb
Dong Li <dongli@vt.edu>
2023-10-20 16:08:03 -0400

    update legends

--- CompareWithBaseline/DrawOnlyLegends.py
+++ CompareWithBaseline/DrawOnlyLegends.py
@@ -22,13 +22,12 @@ for i in range(method_num):
                          ], label=baseline_method_labels[method_names[i]],
                          markersize=marker_size_map[method_names[i]])
 
-plt.legend( loc='lower center',  ncol=method_num)
+plt.legend( loc='lower center',  ncol=method_num, fontsize=14)
 # x0,x1 = 0.86124, 1.138
-x0,x1 = 0.844, 1.156
-delta_y=0.0429
-y0,y1 = 0.90+delta_y+0.0052, 1-delta_y-0.0036
+x0,x1 = 0.782, 1.218
+y0,y1 = 0.948, 0.958
 bbox = Bbox([[x0,y0],[x1,y1]])
 bbox = bbox.transformed(ax.transData).transformed(fig.dpi_scale_trans.inverted())
 plt.savefig(ROOT_CompareWithBaseline_PATH+"legends.pdf", format='pdf', bbox_inches=bbox)
-# plt.show(block=False)
-# plt.show()
+plt.show(block=False)
+plt.show()
