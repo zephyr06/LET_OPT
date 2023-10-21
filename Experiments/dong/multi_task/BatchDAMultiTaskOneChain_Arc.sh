9c1fa6bdf252ca8e2a2490c11b1cb4c1351c7fa0
Dong Li <dongli@vt.edu>
2023-10-21 01:16:16 -0400

    update batch scripts

--- Experiments/dong/multi_task/BatchDAMultiTaskOneChain_Arc.sh
+++ Experiments/dong/multi_task/BatchDAMultiTaskOneChain_Arc.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-task_number_list=( 40 39 38 37 36 35 34 33 32 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 )
+task_number_list=( 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 )
 files_per_task_list=( 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 )
 time_per_node_list=( 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10) # in hour
-TOTAL_TASK_NUMBER=35 # the max index, start from 0
-
+TOTAL_TASK_NUMBER=16 # the max index, start from 0
+ThreadPerTask=1
 MinFileIndex=0
 MaxFileIndex=999
 
@@ -14,11 +14,11 @@ perform_optimization() {
 	files_per_task=$2
 	time_per_node=$3
 	output_file_name=log/BatchDAMultiChain_${task_number}-$(date +"%Y%m%d%H%M%S").out
-	echo "sbatch -J DA1_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=$(((MaxFileIndex+files_per_task)/files_per_task)) --cpus-per-task=1 --time=${time_per_node}:0:0 --output=${output_file_name} SBatchDAMultiTaskOneChain.sh $1 $MinFileIndex $files_per_task $MaxFileIndex"
+	echo "sbatch -J DA1_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=1 --cpus-per-task=$(((MaxFileIndex+files_per_task)/files_per_task*ThreadPerTask)) --time=${time_per_node}:0:0 --output=${output_file_name} SBatchDAMultiTaskOneChain.sh $1 $MinFileIndex $files_per_task $MaxFileIndex"
 	## test in local
 	# bash SBatchDAMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
 	## launc on ARC
-	sbatch -J DA1_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=$(((MaxFileIndex+files_per_task)/files_per_task)) --cpus-per-task=1 --time=${time_per_node}:0:0 --output=${output_file_name} SBatchDAMultiTaskOneChain.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
+	# sbatch -J DA1_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=1 --cpus-per-task=$(((MaxFileIndex+files_per_task)/files_per_task*ThreadPerTask)) --time=${time_per_node}:0:0 --output=${output_file_name} SBatchDAMultiTaskOneChain.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
 }
 
 
@@ -28,6 +28,6 @@ cd $ROOT_PATH/Experiments/dong/multi_task
 mkdir log
 
 for task_number_index in $(seq 0 1 $TOTAL_TASK_NUMBER); do
-	echo "Processing N=${task_number_list[task_number_index]} with ${files_per_task_list[task_number_index]} files per task:"
-	perform_optimization ${task_number_list[task_number_index]} ${files_per_task_list[task_number_index]} ${time_per_node_list[task_number_index]}
+	echo "Processing N=${task_number_list[task_number_index]} with $((files_per_task_list[task_number_index]*ThreadPerTask)) files per task:"
+	perform_optimization ${task_number_list[task_number_index]} $((files_per_task_list[task_number_index]*ThreadPerTask)) $((time_per_node_list[task_number_index]*ThreadPerTask))
 done
--- Experiments/dong/multi_task/BatchSFMultiTask_Arc.sh
+++ Experiments/dong/multi_task/BatchSFMultiTask_Arc.sh
@@ -5,7 +5,7 @@ task_number_list=( 9 8 7 6 5 4 3 2 )
 files_per_task_list=( 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 )
 time_per_node_list=( 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 ) # in hour
 TOTAL_TASK_NUMBER=7 # the max index start from 0
-
+ThreadPerTask=1
 MinFileIndex=0
 MaxFileIndex=999
 
@@ -15,11 +15,11 @@ perform_optimization() {
 	files_per_task=$2
 	time_per_node=$3
 	output_file_name=log/BatchSF_${task_number}-$(date +"%Y%m%d%H%M%S").out
-	echo "sbatch -J SF_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=$(((MaxFileIndex+files_per_task)/files_per_task)) --cpus-per-task=1 --time ${time_per_node}:0:0 --output=${output_file_name} SBatchSFMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex"
+	echo "sbatch -J SF_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=1 --cpus-per-task=$(((MaxFileIndex+files_per_task)/files_per_task*ThreadPerTask)) --time ${time_per_node}:0:0 --output=${output_file_name} SBatchSFMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex"
 	## test in local
 	# bash SBatchRTMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
 	## launc on ARC
-	sbatch -J SF_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=$(((MaxFileIndex+files_per_task)/files_per_task)) --cpus-per-task=1 --time ${time_per_node}:0:0 --output=${output_file_name} SBatchSFMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
+	sbatch -J SF_${task_number}_LET_Batch --nodes=1 --ntasks-per-node=1 --cpus-per-task=$(((MaxFileIndex+files_per_task)/files_per_task*ThreadPerTask)) --time ${time_per_node}:0:0 --output=${output_file_name} SBatchSFMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
 }
 
 
@@ -29,6 +29,6 @@ cd $ROOT_PATH/Experiments/dong/multi_task
 mkdir log
 
 for task_number_index in $(seq 0 1 $TOTAL_TASK_NUMBER); do
-	echo "Processing N=${task_number_list[task_number_index]} with ${files_per_task_list[task_number_index]} files per task:"
-	perform_optimization ${task_number_list[task_number_index]} ${files_per_task_list[task_number_index]} ${time_per_node_list[task_number_index]}
+	echo "Processing N=${task_number_list[task_number_index]} with $((files_per_task_list[task_number_index]*ThreadPerTask)) files per task:"
+	perform_optimization ${task_number_list[task_number_index]} $((files_per_task_list[task_number_index]*ThreadPerTask)) $((time_per_node_list[task_number_index]*ThreadPerTask))
 done
